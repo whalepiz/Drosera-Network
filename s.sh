9cec603e433450a0352ebeb737b37d074eb57d03
whalepiz <97138501+whalepiz@users.noreply.github.com>
2025-05-10 07:02:16 +0700

    Update s.sh

--- s.sh
+++ s.sh
@@ -43,7 +43,7 @@ source /root/.bashrc
 clear
 
 # Display Crypton header
-figlet -f big "Crypton"
+figlet -f big "Crypton" | lolcat
 
 # Decorative border
 echo -e "\e[36m╔═══════════════════════════════════════════════════════════════╗\e[0m"
